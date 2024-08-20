a4317f97ca34d6f372ed3f987858c5c2335f9c40
DinoSage <dinosage5632@gmail.com>
2024-08-20 14:32:30 -0400

    +Remaining Officers Added

--- src/components/Officers.jsx
+++ src/components/Officers.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
-import img from '../assets/img/people/Ansh.jpg'
+import { officers } from '../constants'
+
 
 const Officers = () => {
+    return(
+        <>
+        <div className='flex flex-row flex-wrap justify-center w-9/12 self-center'>
+            {officers.map((item, index) => (
+                <OfficerCard officer={item} />
+            ))}
+        </div>
+        
+        </>
+    )
+}
+
+const OfficerCard = (props) => {
   return (
     <>
     <div className='flex flex-col items-center w-80 border-4 rounded-3xl p-5 gap-2'>
-        <p className='text-4xl'> Ansh Agrawal</p>
-        <img className='object-cover rounded-full w-40 h-40' src={img} />
-        <p className='text-2xl'> Programming Lead </p>
+        <p className='text-4xl'> {props.officer.name} </p>
+        <img className='object-cover rounded-full w-40 h-40' src={props.officer.img} />
+        <p className='text-2xl'> {props.officer.role} </p>
         <hr className='w-full' />
-        <div className='text-justify'> 
-            Ansh Agrawal is in their third and final year at Purdue, on track to graduate early with a major in Computer Science and minor in Mathematics. This will be his second year in SIGGD and first year as a Programming Lead. He has skills in variety of programming languages and tools. By incorporating his computer science technical ability with game development projects, he aspires to enter the gaming industry and start his own indie game studio some day.
+        <div className='text-justify leading-tight'> 
+            {props.officer.description}
         </div>
     </div>
     </>
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,6 +1,66 @@
+import IvanImg from '../assets/img/people/Ivan.jpg'
+import AndrewImg from '../assets/img/people/Andrew.jpg'
+import AnshImg from '../assets/img/people/Ansh.jpg'
+import NathanImg from '../assets/img/people/Nathan.jpg'
+import JacobImg from '../assets/img/people/Jacob.jpg'
+
+import NoImg from '../assets/img/people/no-image.webp'
+
+
 export const navItems = [
     { label: "About Us", href: "#" },
     { label: "Games", href: "#"},
     { label: "Officers", href: "#"},
     { label: "Game Jams", href: "#"},
+]
+
+export const officers = [
+    { 
+        name: "Ivan Yang",
+        role: "President",
+        startYear: "2022",
+        endYear: "Present",
+        img: IvanImg,
+        description: "Ivan Yang is currently majoring Computer Science and Physics and minoring in Chemistry and Mathematics. This is his third year in SIGGD and his first year as the President. He enjoys watching lots of TV shows and movies. He doesn't spend a lot of time playing games, but he is passionate about creating games!"
+    },
+    { 
+        name: "Andrew Hanlon",
+        role: "Vice President",
+        startYear: "2022",
+        endYear: "Present",
+        img: AndrewImg,
+        description: "Andrew Hanlon is a junior who’s majoring in Biomedical Health Sciences with a Pre-Med concentration. This is his third year in SIGGD and first year on the leadership staff. He’s always had a love for video games and has been working to develop his musical and programming skills during his time in the club. While someday working in the field of medicine is his big focus, he is hopeful that game development can be a hobby for him in the future."
+    },
+    { 
+        name: "Alex",
+        role: "Art Lead",
+        startYear: "2022",
+        endYear: "Present",
+        img: NoImg,
+        description: "TBD"
+    },
+    { 
+        name: "Ansh Agrawal",
+        role: "Programming Lead",
+        startYear: "2023",
+        endYear: "Present",
+        img: AnshImg,
+        description: "Ansh Agrawal is in their third and final year at Purdue, on track to graduate early with a major in Computer Science and minor in Mathematics. This will be his second year in SIGGD and first year as a Programming Lead. He has skills in variety of programming languages and tools. By incorporating his computer science technical ability with game development projects, he aspires to enter the gaming industry and start his own indie game studio some day."
+    },
+    { 
+        name: "Nathan Whitacre",
+        role: "Design Lead",
+        startYear: "2022",
+        endYear: "Present",
+        img: NathanImg,
+        description: "Nathan is a third-year Purdue student majoring in Computer Science and minoring in Spanish Language and Culture. This is his third year with SIGGD and his second year as Design Lead. When he's not jotting down paper designs or coding his next project, you can find him bouldering at the Corec or practicing his Spanish at the Latino Cultural Center. Some of his favorite video games include Overwatch, The Last of Us, and Celeste."
+    },
+    { 
+        name: "Jacob Aldridge",
+        role: "Audio Lead",
+        startYear: "2022",
+        endYear: "Present",
+        img: JacobImg,
+        description: "Jacob is a senior studying Computer Science with concentrations in Graphics, Systems Software, and Algorithmic foundations. This is his fourth year in SIGGD and his first year as Audio Lead. He enjoys drawing and making music and is getting minors in both of those areas. Of course, he also loves to play video games."
+    },
 ]
