7b7f8f7a4a5aafe015a6c782080c0deeb4d36f41
DinoSage <dinosage5632@gmail.com>
2024-08-20 13:56:31 -0400

    First Design of Officer Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Officers from './components/Officers'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Officers />
     </>
   )
 }
