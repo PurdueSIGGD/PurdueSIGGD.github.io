ff2b9a3b00761112312ebdafdf0fcfddad95b702
DinoSage <dinosage5632@gmail.com>
2024-08-20 14:47:51 -0400

    %CSS Touchup of Officers

--- src/components/Officers.jsx
+++ src/components/Officers.jsx
@@ -5,7 +5,7 @@ import { officers } from '../constants'
 const Officers = () => {
     return(
         <>
-        <div className='flex flex-row flex-wrap justify-center w-9/12 self-center'>
+        <div className='flex flex-row flex-wrap justify-center w-9/12 mx-auto gap-5'>
             {officers.map((item, index) => (
                 <OfficerCard officer={item} />
             ))}
@@ -18,12 +18,12 @@ const Officers = () => {
 const OfficerCard = (props) => {
   return (
     <>
-    <div className='flex flex-col items-center w-80 border-4 rounded-3xl p-5 gap-2'>
+    <div className='flex flex-col items-center w-80 border-2 rounded-2xl p-5 gap-2'>
         <p className='text-4xl'> {props.officer.name} </p>
         <img className='object-cover rounded-full w-40 h-40' src={props.officer.img} />
         <p className='text-2xl'> {props.officer.role} </p>
         <hr className='w-full' />
-        <div className='text-justify leading-tight'> 
+        <div className='leading-tight'> 
             {props.officer.description}
         </div>
     </div>
