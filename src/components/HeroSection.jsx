8073b11557009fd607bbea97cfe57ba1c6ec789a
DinoSage <dinosage5632@gmail.com>
2024-08-23 18:01:53 -0400

    +Discord Button to Hero Section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import background from '../assets/img/homepage/SIGGD group image 2022.jpg'
 import acmLogo from '../assets/img/logos/ACMLogo.png'
 import siggdLogo from '../assets/img/logos/SIGGD_logo_final_square.png'
+import { discordLink } from '../constants'
 
 const HeroSection = () => {
   return (
@@ -11,7 +12,8 @@ const HeroSection = () => {
         <div className='absolute hidden lg:flex flex-row top-0 w-full h-full justify-between items-center'>
           <div className='ml-20'>
             <p className='text-8xl font-extrabold'> Purdue SIGGD</p>
-            <p className='text-2xl font-semibold'> Purdue ACM's Special Interest Group for Game Development</p>
+            <p className='text-2xl font-semibold mb-5'> Purdue ACM's Special Interest Group for Game Development</p>
+            <a href={discordLink} className='py-3 px-5 border rounded-md bg-indigo-700'> Join our Discord </a>
           </div>
           <div>
             <div className="mr-52 flex flex-col justify-center">
@@ -24,7 +26,8 @@ const HeroSection = () => {
         <div className='absolute lg:hidden flex flex-col top-0 w-full h-full justify-center items-center'>
           <img className='w-64 object-cover' src={siggdLogo} />
           <p className='text-5xl font-extrabold'> Purdue SIGGD</p>
-          <p className='text-sm font-semibold'> Purdue ACM's Special Interest Group for Game Development</p>
+          <p className='text-sm font-semibold mb-5'> Purdue ACM's Special Interest Group for Game Development</p>
+          <a href={discordLink} className='py-3 px-5 border rounded-md bg-indigo-700'> Join our Discord </a>
           <img className='mt-10 w-64 object-cover' src={acmLogo} />
         </div>
 
