5bcd6fcb71ff2d182f156295a8c6d824d3838265
DinoSage <dinosage5632@gmail.com>
2024-08-23 16:37:54 -0400

    +Missing Officer Details; -Old Card Components

--- src/components/Officers.jsx
+++ src/components/Officers.jsx
@@ -11,7 +11,7 @@ const Officers = () => {
             </h1>
             <div className='flex flex-row flex-wrap justify-center w-9/12 mx-auto gap-5'>
                 {officers.map((item, index) => (
-                    <OfficerCardColor officer={item} />
+                    <OfficerCard officer={item} />
                 ))}
             </div>
         </div>
@@ -19,54 +19,22 @@ const Officers = () => {
     )
 }
 
-const OfficerCardOld = (props) => {
-    return(
-        <>
-        <div className='flex flex-col items-center w-80 border-2 rounded-2xl p-5 gap-2'>
-        <p className='text-4xl'> {props.officer.name} </p>
-        <img className='object-cover rounded-full w-40 h-40' src={props.officer.img} />
-        <p className='text-2xl'> {props.officer.role} </p>
-        <hr className='w-full' />
-        <div className='leading-tight'> 
-            {props.officer.description}
-        </div>
-    </div>
-        </>
-    )
-}
-
-const OfficerCardColor = (props) => {
+const OfficerCard = (props) => {
     return (
       <>
       <div className='flex flex-col items-center w-80 p-5'>
-          <div className='bg-red-600 w-full p-5 flex flex-col rounded-t'>
+          <div className='bg-red-600 w-full p-5 flex flex-col rounded-t-lg'>
               <p className='text-3xl'> {props.officer.name} </p>
               <p className='text-xl italic'> {props.officer.role} </p>
               <img className='object-cover rounded-full w-40 h-40 self-center border-4 border-white' src={props.officer.img} />
           </div>
           
-          <div className='leading-tight bg-black p-5 rounded-b h-96 w-full'> 
+          <div className='leading-tight bg-black p-5 rounded-b-lg w-full'> 
               {props.officer.description}
           </div>
       </div>
       </>
     )
   }
-
-const OfficerCard = (props) => {
-    return(
-        <>
-        <div className='flex flex-col items-center w-80 border-2 rounded-2xl p-5 gap-2'>
-        <p className='text-4xl'> {props.officer.name} </p>
-        <img className='object-cover rounded-full w-40 h-40' src={props.officer.img} />
-        <p className='text-2xl'> {props.officer.role} </p>
-        <hr className='w-full' />
-        <div className='leading-tight'> 
-            {props.officer.description}
-        </div>
-    </div>
-        </>
-    )
-}
 
 export default Officers
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -32,12 +32,12 @@ export const officers = [
         description: "Andrew Hanlon is a junior who’s majoring in Biomedical Health Sciences with a Pre-Med concentration. This is his third year in SIGGD and first year on the leadership staff. He’s always had a love for video games and has been working to develop his musical and programming skills during his time in the club. While someday working in the field of medicine is his big focus, he is hopeful that game development can be a hobby for him in the future."
     },
     { 
-        name: "Alex",
+        name: "Alex Ashby",
         role: "Art Lead",
         startYear: "2022",
         endYear: "Present",
         img: NoImg,
-        description: "TBD"
+        description: "Alex Ashby is a junior majoring in Game Development & Design with a focus on the creative side of game dev. This is their second year in SIGGD and their first year on the leadership team as the Art Lead. They've always loved both video games and the arts and aspire to be a concept artist at a major studio. They enjoy creating art and writing in their free time… oh, and of course, playing games. Their 3 favorite games are Destiny, Terraria and Death Must Die"
     },
     { 
         name: "Ansh Agrawal",
