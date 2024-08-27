20f7c152a371d8433adc6575608049ce0b43b879
DinoSage <dinosage5632@gmail.com>
2024-08-27 17:54:22 -0400

    %Attempted About Us Image

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import AboutUsImg from '../assets/img/aboutusimg.jpg'
 
 const AboutUs = () => {
     return(
@@ -7,12 +8,14 @@ const AboutUs = () => {
             <h1 className="pt-20 my-5 text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
                 About Us
             </h1>
+            <div className='lg:mx-20 lg:text-lg mx-10 flex flex-row items-center'>
+                <p className=''>
+                    Founded in Fall 2011, Purdue ACM's Special Interest Group on Game Development is a multi-disciplinary group of Purdue University students that creates a new game each school year. As part of the Association for Computing Machinery Purdue chapter, we are the first SIGGD of ACM National. Our goal is to develop creative and interesting games. In addition, we host a game jam (game development hackathon) each semester were groups come together either online or in-person to create a video game prototype in 48 hours. <br/> <br/>
 
-            <p className='lg:mx-20 lg:text-lg mx-10'>
-                Founded in Fall 2011, Purdue ACM's Special Interest Group on Game Development is a multi-disciplinary group of Purdue University students that creates a new game each school year. As part of the Association for Computing Machinery Purdue chapter, we are the first SIGGD of ACM National. Our goal is to develop creative and interesting games. In addition, we host a game jam (game development hackathon) each semester were groups come together either online or in-person to create a video game prototype in 48 hours. <br/> <br/>
-
-                We primarily communicate through our Discord Server, so if you are interested in joining the club be sure to join!
-            </p>
+                    We primarily communicate through our Discord Server, so if you are interested in joining the club be sure to join!
+                </p>
+            </div>
+            
         </div>
         </>
     )
