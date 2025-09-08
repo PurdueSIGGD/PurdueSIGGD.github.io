52fec785fe5ea3c6d8d14a3cf53439f24aacebc9
WangXin1507 <simonwang421@outlook.com>
2025-09-08 01:03:23 -0400

    updated Harry and Andrew info

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -2,7 +2,7 @@ import IvanImg from '../assets/img/people/Ivan.jpg'
 import AndrewImg from '../assets/img/people/Andrew.jpg'
 import AnshImg from '../assets/img/people/Ansh.jpg'
 import NathanImg from '../assets/img/people/Nathan.jpg'
-import JacobImg from '../assets/img/people/Jacob.jpg'
+import HarryImg from '../assets/img/people/Harry.jpg'
 
 import NoImg from '../assets/img/people/no-image.webp'
 
@@ -37,12 +37,12 @@ export const officers = [
         description: "Ivan Yang is currently majoring Computer Science and Physics and minoring in Chemistry and Mathematics. This is his third year in SIGGD and his first year as the President. He enjoys watching lots of TV shows and movies. He doesn't spend a lot of time playing games, but he is passionate about creating games!"
     },
     { 
-        name: "Andrew Hanlon",
+        name: "Harry Weinberger",
         role: "Vice President",
-        startYear: "2022",
+        startYear: "2024",
         endYear: "Present",
-        img: AndrewImg,
-        description: "Andrew Hanlon is a junior who’s majoring in Biomedical Health Sciences with a Pre-Med concentration. This is his third year in SIGGD and first year on the leadership staff. He’s always had a love for video games and has been working to develop his musical and programming skills during his time in the club. While someday working in the field of medicine is his big focus, he is hopeful that game development can be a hobby for him in the future."
+        img: HarryImg,
+        description: "Harry is a second-year Purdue student majoring in Business Analytics and Information Management, as well as Creative Writing. This is his second year with SIGGD. He loves watching movies, TV shows, youtube videos, and really anything that moves on a screen. He has always loved video games since playing Super Smash Bros with his brother as a kid, and hopes to one day put that interest into making games of his own as a career. Some of his favorite games include Calamity Terraria, Elden Ring, and Ori and the Will of the Wisps."
     },
     { 
         name: "Alex Ashby",
@@ -69,12 +69,12 @@ export const officers = [
         description: "Nathan is a third-year Purdue student majoring in Computer Science and minoring in Spanish Language and Culture. This is his third year with SIGGD and his second year as Design Lead. When he's not jotting down paper designs or coding his next project, you can find him bouldering at the Corec or practicing his Spanish at the Latino Cultural Center. Some of his favorite video games include Overwatch, The Last of Us, and Celeste."
     },
     { 
-        name: "Jacob Aldridge",
+        name: "Andrew Hanlon",
         role: "Audio Lead",
         startYear: "2022",
         endYear: "Present",
-        img: JacobImg,
-        description: "Jacob is a senior studying Computer Science with concentrations in Graphics, Systems Software, and Algorithmic foundations. This is his fourth year in SIGGD and his first year as Audio Lead. He enjoys drawing and making music and is getting minors in both of those areas. Of course, he also loves to play video games."
+        img: AndrewImg,
+        description: "Andrew Hanlon is a Senior who's majoring in Biomedical Health Sciences with a Pre-Med concentration. He has been a part of SIGGD all four years that he's been in college and has developed his musical skills while also picking up on some programming skills. He's a completionist and has had a love for video games his whole life. While he is focused on working in the medical field someday, he is hopeful that game development can be a hobby for him in the future. His favorite games include Terraria, Pokemon, and Hollow Knight."
     },
 ]
 
